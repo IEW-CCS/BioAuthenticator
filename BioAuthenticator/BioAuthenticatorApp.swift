1b403e2d379f31f569002c2d0b0a0559e6dcdd9a
James <robohood83@gmail.com>
2022-08-16 07:45:07 +0800

    Initial Commit

---
+++ BioAuthenticator/BioAuthenticatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  BioAuthenticatorApp.swift
+//  BioAuthenticator
+//
+//  Created by Lo Fang Chou on 2022/8/16.
+//
+
+import SwiftUI
+
+@main
+struct BioAuthenticatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
